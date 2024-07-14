0b24745bdfc24c4642dcdc67819472ff7d7b6e7b
Jhosef <jmperezperez25@gmail.com>
2024-07-14 19:12:36 -0400

    autoencoder model

--- generator/edge_detection.py
+++ generator/edge_detection.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-def image_edge_detection(image_path, train=True):
+def image_edge_detection(image_path, train=True , replacement=""):
     
     # Load the image
     try:
@@ -11,6 +11,8 @@ def image_edge_detection(image_path, train=True):
     
     if image is None:
         return 
+    
+    image = cv2.resize( image , (1026,1026) )
     
     # Apply Gaussian blur to reduce noise
     blurred = cv2.GaussianBlur(image, (1, 1), 1)
@@ -24,15 +26,29 @@ def image_edge_detection(image_path, train=True):
     # Threshold the image to get binary edges
     _, binary_edges = cv2.threshold(normalized_laplacian, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     
+    number_of_white = 0
+    number_of_black = 0
+    for i in range(len(binary_edges)):
+        for j in range( len(binary_edges[i])):
+            
+            if binary_edges[i,j] > 128:
+                binary_edges[i,j] = 255
+                number_of_white += 1
+            else:
+                binary_edges[i,j] = 0
+                number_of_black += 1
+    
+    if number_of_white < number_of_black:
+        binary_edges = cv2.bitwise_not(binary_edges)
+    
     # Define the output filename
-    output_filename = image_path.split('/')[-1]  # Modify this as needed
     
     if train:
-        print("./"+ output_filename)
-        cv2.imwrite("./" + output_filename, binary_edges)
+        print("./"+ replacement)
+        cv2.imwrite("./" + replacement, binary_edges)
     else:
-        print("./"+ output_filename)
-        cv2.imwrite("./" + output_filename, binary_edges)
+        print("./"+ replacement)
+        cv2.imwrite("./" + replacement, binary_edges)
 
 # Example usage
 
@@ -46,12 +62,13 @@ len_val_path = len(os.listdir(val_path))
 # for i in range(len_train_path):
     
 #     if i < 9:
-#         image_edge_detection(f"{train_path}00{ i + 1 }.jpg")
+#         image_edge_detection(f"{train_path}00{ i + 1 }.jpg" , replacement=f"{i}.jpg" )
 #     if i >= 10 and i < 99:
-#         image_edge_detection(f"{train_path}0{i + 1 }.jpg")
+#         image_edge_detection(f"{train_path}0{i + 1 }.jpg" , replacement=f"{i}.jpg" )
 #     if  i >= 100:
-#         image_edge_detection(f"{train_path}{i + 1}.jpg")
+#         image_edge_detection(f"{train_path}{i + 1}.jpg" , replacement=f"{i}.jpg" )
+
 
 # save the validation dataset 
 for i in range(len_val_path):
-    image_edge_detection( f"{val_path}{ i + 641 }.jpg" , False )
+    image_edge_detection( f"{val_path}{ i + 641 }.jpg" , False , replacement=f"{i}.jpg" )
--- generator/generator.py
+++ generator/generator.py
@@ -1,162 +1,126 @@
-from keras import Sequential
-from keras.layers import Dense
-from keras.preprocessing.image import ImageDataGenerator
+import keras
+import tensorflow as tf
 from keras import layers
 from keras import Input
 from keras import Model
+import numpy as np
+import cv2
+import matplotlib as plt
 
 import os
 os.system("cls")
 
+def autoencoder(input_img):
+    
+    # Encoder
+    x = layers.MaxPooling3D(pool_size=(38, 1, 1 ))(input_img) #5 x 5 x 64
+    x = layers.Conv3D(8 , (3,3,3) , padding='same', activation='relu')(x) #30 x 30 x 32
+    x = layers.Dense(32 , activation='relu')(x) #30 x 30 x 32
+    
+    x = layers.MaxPooling3D(pool_size=(2, 3 , 3 ))(x) #15 x 15 x 32
+    x = layers.Conv3D(4 , (3,3,3) , padding='same', activation='relu')(x) #30 x 30 x 32
+    x = layers.Dense(16 , activation='relu')(x) #15 x 15 x 64
+    
+    
+    x = layers.MaxPooling3D(pool_size=(1, 3 , 3 ))(x) #15 x 15 x 32
+    x = layers.Conv3D(2 , (3,3,3) , padding='same', activation='relu')(x) #30 x 30 x 32
+    encode = layers.Dense(8 , activation='relu')(x) #15 x 15 x 64
+    
+    #decoder
+    x = layers.UpSampling3D((1,3,3))(encode) # 15 x 15 x 128
+    x = layers.Conv3D(2 , (3,3,3) , padding='same' , activation='relu' )(x)
+    x = layers.Dense(8, activation='relu')(x) # 15 x 15 x 64
+    x = layers.UpSampling3D((2,3,3))(x) # 30 x 30 x 64
+    
+    x = layers.Conv3D(4 , (2,2,2) , padding='same' , activation='relu' )(x)
+    x = layers.Dense(16, activation='relu')(x) # 15 x 15 x 64
+    x = layers.UpSampling3D((38,1,1))(x) # 30 x 30 x 64
+    decoded = layers.Dense(1, activation='relu')(x) # 30 x 30 x 1
+    
+    return decoded    
+
 def dataset_train_validator():
     
-    train_dir = '../dataset/train'
-    validation_dir = '../dataset/validation'
+    train_dir = './img_map_dataset/'
     
-    train_datagen = ImageDataGenerator(rescale=1./255)
-    validation_datagen = ImageDataGenerator(rescale=1./255)
-    
-#     len_train = len(os.listdir(train_dir + "/earth/" ) )
-#     len_val = len(os.listdir(validation_dir + "/earth/" ) )
-    
-
-    train_generator = train_datagen.flow_from_directory(
-            train_dir,
-            target_size=(128, 128),
-            batch_size=32,
-            class_mode='input')
-
-    validation_generator = validation_datagen.flow_from_directory(
-            validation_dir,
-            target_size=(128, 128),
-            batch_size=32,
-            class_mode='input')
-
-    return train_generator , validation_generator
-
-def classfier( y_true, y_pred ):
-        os.system("cls")
-        print(y_pred)
+    aux = []
+    print("\033[1;32m loading training data \033[0m")
+    for img in range( len(os.listdir(train_dir))):
         
-        '''
-        loss: Loss function. May be a string : name of loss function , or
-        a tf.keras.losses.Loss instance. See tf.keras.losses. A loss function is any callable with the signature loss = fn(y_true, y_pred), 
-        where y_true are the ground truth values, and y_pred are the model's predictions. 
-        y_true should have shape (batch_size, d0, .. dN) (except in the case of sparse loss functions such as sparse categorical crossentropy which 
-        expects integer arrays of shape (batch_size, d0, .. dN-1)). y_pred should have shape (batch_size, d0, .. dN). The loss function should return 
-        a float tensor. If a custom Loss instance is used and reduction is set to None, return value has shape (batch_size, d0, .. dN-1) i.e. per-sample
-        or per-timestep loss values; otherwise, it is a scalar. If the model has multiple outputs, you can use a different loss on each output by passing 
-        a dictionary or a list of losses. The loss value that will be minimized by the model will then be the sum of all individual losses, unless loss_weights is specified.
-
-        Configures the model for training.
-
-        Example:
-
-        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
-                loss=tf.keras.losses.BinaryCrossentropy(),
-                metrics=[tf.keras.metrics.BinaryAccuracy(),
-                        tf.keras.metrics.FalseNegatives()])
-        Args
-        optimizer: String : name of optimizer or optimizer instance. See
-        tf.keras.optimizers.
-
-        metrics : List of metrics to be evaluated by the model during
-        training and testing. Each of this can be a string (name of a built-in function), function or a tf.keras.metrics.Metric instance. See tf.keras.metrics. 
-        Typically you will use metrics=['accuracy']. A function is any callable with the signature result = fn(y_true, y_pred). To specify different metrics for 
-        different outputs of a multi-output model, you could also pass a dictionary, such as metrics={'output_a':'accuracy', 'output_b':['accuracy', 'mse']}. You
-        can also pass a list to specify a metric or a list of metrics for each output, such as metrics=[['accuracy'], ['accuracy', 'mse']] or metrics=['accuracy',
-        ['accuracy', 'mse']]. When you pass the strings 'accuracy' or 'acc', we convert this to one of tf.keras.metrics.BinaryAccuracy, tf.keras.metrics.CategoricalAccuracy, 
-        tf.keras.metrics.SparseCategoricalAccuracy based on the shapes of the targets and of the model output. We do a similar conversion for the strings 'crossentropy' and 
-        'ce' as well. The metrics passed here are evaluated without sample weighting; if you would like sample weighting to apply, you can specify your metrics via the 
-        weighted_metrics argument instead.
-
-        loss_weights : Optional list or dictionary specifying scalar
-        coefficients (Python floats) to weight the loss contributions of different model outputs. The loss value that will be minimized by the model will then be the weighted
-        sum of all individual losses, weighted by the loss_weights coefficients. If a list, it is expected to have a 1:1 mapping to the model's outputs. If a dict, it is 
-        expected to map output names (strings) to scalar coefficients.
-
-        weighted_metrics : List of metrics to be evaluated and weighted by
-        sample_weight or class_weight during training and testing.
-
-        run_eagerly : Bool. If True, this Model's logic will not be
-        wrapped in a tf.function. Recommended to leave this as None unless your Model cannot be run inside a tf.function. run_eagerly=True is not supported when using 
-        tf.distribute.experimental.ParameterServerStrategy. Defaults to False.
-
-        steps_per_execution : Int or 'auto'. The number of batches to
-        run during each tf.function call. If set to "auto", keras will automatically tune steps_per_execution during runtime. Running multiple batches inside a single
-        tf.function call can greatly improve performance on TPUs, when used with distributed strategies such as ParameterServerStrategy, or with small models with a 
-        large Python overhead. At most, one full epoch will be run each execution. If a number larger than the size of the epoch is passed, the execution will be truncated
-        to the size of the epoch. Note that if steps_per_execution is set to N, Callback.on_batch_begin and Callback.on_batch_end methods will only be called every N batches
-        (i.e. before/after each tf.function execution). Defaults to 1.
-
-        jit_compile : If True, compile the model training step with XLA.
-        XLA is an optimizing compiler for machine learning. jit_compile is not enabled for by default. Note that jit_compile=True may not necessarily work for all models. 
-        For more information on supported operations please refer to the XLA documentation. Also refer to known XLA issues for more details.
-
-        pss_evaluation_shards : Integer or 'auto'. Used for
-        tf.distribute.ParameterServerStrategy training only. This arg sets the number of shards to split the dataset into, to enable an exact visitation guarantee for evaluation,
-        meaning the model will be applied to each dataset element exactly once, even if workers fail. The dataset must be sharded to ensure separate workers do not process the 
-        same data. The number of shards should be at least the number of workers for good performance. A value of 'auto' turns on exact evaluation and uses a heuristic for the 
-        number of shards based on the number of workers. 0, meaning no visitation guarantee is provided. NOTE: Custom implementations of Model.test_step will be ignored when doing 
-        exact evaluation. Defaults to 0.
-
-        **kwargs : Arguments supported for backwards compatibility only.
+        image = cv2.imread(train_dir + f"/map{img}.jpg", cv2.IMREAD_GRAYSCALE)
         
-        '''
+        if image is None:
+            continue
+        
+        image = image.astype(np.float32) / 255
+        aux.append(image.reshape( 1444,27,27 ) )
+    
+    data = np.stack( aux, axis=0)
 
-        return 
+    num_train = int(len(os.listdir(train_dir)) * 0.8)
+    
+    train_data = data[ 0 : num_train ]
+    val_data = data[ num_train: ]
+    
+    
+    return train_data , val_data
 
-def generator():
+def engine():
     
     hyper_params = []
     
-    # Input shape: (batch_size, 28, 28, 1)
-    input_img = Input(shape=(28, 28, 1))
-
-    # Encoder
-    x = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
-    x = layers.MaxPooling2D((2, 2), padding='same')(x)
-    x = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-    encoded = layers.MaxPooling2D((2, 2), padding='same')(x)
-
-    # Decoder
-    x = layers.Conv2DTranspose(64, kernel_size=(2, 2), strides=(2, 2), padding='same')(encoded)
-    x = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-    decoded = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+    print("\033[1;32m building model \033[0m")
     
-    # Classifier head    
-    x = layers.Flatten()(decoded)  # Flatten the output of the decoder
-    dense_classifier = Dense( 1026 * 1026 , activation='softmax')(x)  # Add a dense layer for dimensionality reduction
+    train_data , val_data = dataset_train_validator()
+    
+    # Input shape: (batch_size, 28, 28, 1)
+    input_img = Input(shape=( 1444 , 27, 27, 1 ))
 
     # Create a new model for classification
-    classifier_model = Model(inputs=input_img, outputs=dense_classifier)
-        
+    auto_encoder = Model(inputs=input_img, outputs=autoencoder(input_img))
     
     # Summary of the model
-    classifier_model.summary()
+    auto_encoder.summary()
 
     # Compile the model
-    classifier_model.compile(optimizer='adam',
-                             loss=classfier,
+    
+    print("\033[1;32m compiling model \033[0m")
+    
+    auto_encoder.compile(optimizer= 'Adam',
+                             loss='mean_squared_error',
                              metrics=['accuracy'])
 
-    train_generator , validation_generator = dataset_train_validator()
+    epochs = 5
     
-    epoch = 10
+    print("\033[1;32m fitting model \033[0m")
     
-    history = classifier_model.fit(
-        
-        train_generator,
-        epochs=epoch,  # Number of epochs
-        steps_per_epoch=len(train_generator),
-        validation_data=validation_generator,
-        validation_steps=len(validation_generator),
-    )
-
-#     test_loss, test_acc = autoencoder.evaluate(test_generator, test_labels)
+    autoencoder_train = auto_encoder.fit(train_data , train_data , batch_size=1,epochs=epochs,verbose=1,validation_data=(val_data,val_data))
     
-#     print('\nTest accuracy:', test_acc)
+    auto_encoder.save('autoencoder.h5')  # Creates a HDF5 file 'my_model.h5'
     
     pass
 
-generator()
+def visualize_training( autoencoder_train ):
+
+    loss = autoencoder_train.history['loss']
+    val_loss = autoencoder_train.history['val_loss']
+    epochs = range(epochs)
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+def use_model():
+    
+    test_dir = './img_map_dataset/'
+    
+    for img in range( len(os.listdir(test_dir))):
+    
+        test = cv2.imread('./dataset_balck_white/train/0.jpg').reshape(1444,27,27)
+        model = keras.load('autoencoder.h5')
+    
+    model.predict(test)
+    
+    pass
