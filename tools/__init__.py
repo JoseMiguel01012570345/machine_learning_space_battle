61110c4603441aca703f9ea624107e5d1156cb3c
Jhosef <jmperezperez25@gmail.com>
2024-08-20 17:10:32 -0400

    refactoring

--- tools/__init__.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def matrix_or_matrix(a,b):
-    """
-    returns the matrix a or b, this is, a binary matrix where each component (i,j) is 1 if a[i,j] or
-    b[i,j] are equal to 1, else 0
-    """
-    return np.sign(a + b)
